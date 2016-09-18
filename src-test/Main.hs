78f420cceb46815fb73d1232a52860b6e587d6c2
joranvar <bart.post@gmail.com>
2016-09-18 18:05:29 +0200

    Any game can be scored

--- src-test/Main.hs
+++ src-test/Main.hs
@@ -5,11 +5,11 @@
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.SmallCheck
---import Test.SmallCheck.Series
+import Test.SmallCheck.Series
 
-import Lib ( )
--- instance (Monad m) => Serial m T where
---   series = generate (\d -> take d [ ])
+import Lib (score, Player(..), Score(..))
+instance (Monad m) => Serial m Player where
+  series = generate (\d -> take d [Player])
 
 main :: IO ()
 main = defaultMain $ testGroup "all-tests" tests
@@ -22,8 +22,8 @@ tests =
 
 scTests :: [TestTree]
 scTests =
-  [ testProperty "id x == x" $
-    \x -> id (x::Int) == x
+  [ testProperty "Any game can be scored" $
+    \wins -> score wins == Score
   ]
 
 huTests :: [TestTree]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,20 @@
 -- | TennisKata - http://www.codingdojo.org/cgi-bin/index.pl?KataTennis
 module Lib
   (
+    -- * Exported types
+    Player(..)
+  , Score(..)
     -- * Exported functions
+  , score
   ) where
+
+-- | A player in the game of Tennis
+data Player = Player
+  deriving Show
+-- | The score at any point in the game of Tennis
+data Score = Score
+  deriving Eq
+
+score :: [Player] -- ^ The players winning a ball
+      -> Score    -- ^ The score after all the wins
+score _ = Score

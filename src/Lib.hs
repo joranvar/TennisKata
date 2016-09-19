e16a29acbac196bf9a79c5814eb5b6f7bed178ce
joranvar <bart.post@gmail.com>
2016-09-19 07:12:32 +0200

    Define the domain types

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,16 @@
 -- | TennisKata - http://www.codingdojo.org/cgi-bin/index.pl?KataTennis
 module Lib
   (
+    -- * Domain types
+    Score(..)
+  , Player(..)
+  , Point(..)
     -- * Exported functions
   ) where
+
+-- | The state of the game of Tennis
+data Score = LoveAll | Points Point Point | Advantage | Deuce | Winner Player
+-- | The point score
+data Point = Love | Fifteen | Thirty | Forty
+-- | The player
+data Player = Player1 | Player2

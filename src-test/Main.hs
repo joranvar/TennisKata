f511dc0d2715aa9474bb7c44146518b7237b3d50
joranvar <bart.post@gmail.com>
2016-09-20 07:33:13 +0200

    At least one ball: no love all (fail)

--- src-test/Main.hs
+++ src-test/Main.hs
@@ -5,11 +5,11 @@
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.SmallCheck
---import Test.SmallCheck.Series
+import Test.SmallCheck.Series
 
 import Lib
--- instance (Monad m) => Serial m T where
---   series = generate (\d -> take d [ ])
+instance (Monad m) => Serial m Player where
+  series = generate (\d -> take d [Player1, Player2])
 
 main :: IO ()
 main = defaultMain $ testGroup "all-tests" tests
@@ -22,8 +22,8 @@ tests =
 
 scTests :: [TestTree]
 scTests =
-  [ testProperty "id x == x" $
-    \x -> id (x::Int) == x
+  [ testProperty "Scoring at least one ball: no longer Love all" $
+    \ball balls -> score (ball:balls) /= Points Love Love
   ]
 
 huTests :: [TestTree]

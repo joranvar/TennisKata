e7053e2a1216c6975986cae1200d516e1142edb3
joranvar <bart.post@gmail.com>
2016-09-20 07:28:09 +0200

    Define types

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,21 @@
 -- | TennisKata - http://www.codingdojo.org/cgi-bin/index.pl?KataTennis
 module Lib
   (
-    -- * Exported functions
+    -- * Exported types
+    Score(..)
+  , Player(..)
+  , Point(..)
   ) where
+
+-- | Possible points when not deuce or advantage
+data Point = Love | Fifteen | Thirty | Forty
+
+-- | The player
+data Player = Player1 | Player2
+
+-- | The current score
+data Score = Winner Player
+           | Advantage Player
+           | Deuce
+           | Points { player1::Point
+                    , player2::Point }

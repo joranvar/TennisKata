a48246637695030e3e4864e18564aa54e9d2a558
joranvar <bart.post@gmail.com>
2016-09-21 07:14:03 +0200

    Define the domain

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,23 @@
 -- | TennisKata - http://www.codingdojo.org/cgi-bin/index.pl?KataTennis
 module Lib
   (
-    -- * Exported functions
+    -- * Exported types
+    Score(..)
+  , Point(..)
+  , Player(..)
   ) where
+
+-- | The players
+data Player = Player1 | Player2
+  deriving (Eq, Show)
+
+-- | The point system
+data Point = Love | Fifteen | Thirty | Forty
+  deriving (Eq, Show)
+
+-- | The current score
+data Score = Game Player
+           | Advantage Player
+           | Deuce
+           | Points Point Point
+  deriving (Eq, Show)

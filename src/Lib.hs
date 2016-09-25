187d2207092056be54d10756d941231cc03e9aa3
joranvar <bart.post@gmail.com>
2016-09-25 05:56:57 +0200

    Define domain types for step 1
    Each player can have either of these points in one game 0 15 30 40

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,20 @@
 -- | TennisKata - http://www.codingdojo.org/cgi-bin/index.pl?KataTennis
 module Lib
   (
-    -- * Exported functions
+    -- * Domain types
+    Player (..)
+  , Points (..)
+  , Game (..)
   ) where
+
+-- | Each player
+data Player = Player1 | Player2
+  deriving (Eq, Show, Enum, Bounded)
+
+-- | can have either of these points
+data Points = Love | Fifteen | Thirty | Forty
+  deriving (Eq, Show, Enum, Bounded)
+
+-- | in one game
+data Game = Game { player1 :: Points
+                 , player2 :: Points }

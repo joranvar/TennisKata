90e9cc69f82cf63725a0a6379096a422aa675473
joranvar <bart.post@gmail.com>
2016-09-28 07:25:43 +0200

    Define score as two points

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,11 @@
 -- | TennisKata - http://www.codingdojo.org/cgi-bin/index.pl?KataTennis
 module Lib
   (
-    -- * Exported functions
+    -- * Domain types
+    Score(..)
+  , Point(..)
   ) where
+
+data Score = Score Point Point
+
+data Point = Love | Fifteen | Thirty | Forty

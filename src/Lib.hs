ee010a9aa7bcc0b44eccc881d973a532f39a3e8e
joranvar <bart.post@gmail.com>
2016-09-30 07:25:36 +0200

    Describe end condition

--- src/Lib.hs
+++ src/Lib.hs
@@ -48,5 +48,5 @@ score (Advantage p) winner | winner == p = Winner winner
 -- | Non-written rule: score advances points for winner
 score (Points p1 p2) Player1 = Points (succ p1) p2
 score (Points p1 p2) Player2 = Points p1 (succ p2)
-
-score _ _ = undefined
+-- | Non-written rule: score does not change if game is over
+score (Winner winner) _ = Winner winner

72ea51749b251699361bcca10201494dd12585e0
joranvar <bart.post@gmail.com>
2016-09-30 07:10:04 +0200

    Describe domain type: Player

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,10 @@
 -- | TennisKata - http://www.codingdojo.org/cgi-bin/index.pl?KataTennis
 module Lib
   (
-    -- * Exported functions
+    -- * Domain description
+    Player(..)
   ) where
+
+-- | Each player
+data Player = Player1 | Player2
+  deriving(Eq, Enum, Bounded, Show)
